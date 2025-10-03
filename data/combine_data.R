4e90c83561d926638d8b9a7b249e4033ff0f254d
jii1n <yj5596901@gmail.com>
2025-10-03 13:22:43 +0900

    Add files via upload

---
+++ data/combine_data.R
@@ -0,0 +1,40 @@
+library(tximport)
+library(biomaRt)
+library(tidyverse)
+
+# Kallisto의 transcript-level 결과(abundance.tsv)를 gene-level count 데이터로 변환한 후, 모든 샘플을 하나의 count matrix로 결합하고 저장하는 과정
+mart <- useEnsembl(biomart = "ensembl",
+                   dataset = "celegans_gene_ensembl",
+                   version = "100")
+
+Tx.worm <- getBM(attributes=c('ensembl_transcript_id',
+                              'ensembl_gene_id'),
+                 mart = mart)
+
+Tx.worm <- as_tibble(Tx.worm)
+#we need to rename the two columns we just retrieved from biomart
+Tx.worm <- dplyr::rename(Tx.worm, target_id = ensembl_transcript_id, 
+                         gene_name = ensembl_gene_id)
+bioprojects <- c()
+raw_data <- c()
+for (dir in list.files("../query_download_process_data/ready_for_analysis/")) {
+    path <- file.path("../query_download_process_data/ready_for_analysis", dir, 
+                      list.files(paste(
+                        "../query_download_process_data/ready_for_analysis/", 
+                        dir, sep='')), 'abundance.tsv')
+    
+    Txi_gene <- tximport(path, 
+                         type = "kallisto", 
+                         tx2gene = Tx.worm, 
+                         txOut = FALSE,
+                         ignoreTxVersion = FALSE)
+    
+    genes <- rownames(Txi_gene[["counts"]])
+    samples = lapply(strsplit(path, "/"), '[', 5)
+    colnames(Txi_gene[["counts"]]) <- samples
+    bioprojects <- c(bioprojects, rep(dir, ncol(Txi_gene[["counts"]])))
+    raw_data <- cbind(raw_data, Txi_gene[['counts']])
+}
+
+save(raw_data, file = "../common_datastore/combined_studies_raw_counts.Rdata") 
+save(bioprojects, file = "../common_datastore/bioprojects.Rdata") 
